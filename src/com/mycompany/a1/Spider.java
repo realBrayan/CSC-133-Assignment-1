abbca501a31680331dff49d293ce174a90c08bda
Brayan Mejia Nunez <132964348+realBrayan@users.noreply.github.com>
2025-09-21 20:05:34 -0700

    Updated Spider class. It should be good now. If errors, check move() class.

--- src/com/mycompany/a1/Spider.java
+++ src/com/mycompany/a1/Spider.java
@@ -1,29 +1,36 @@
 package com.mycompany.a1;
-
+import com.codename1.charts.models.Point;
 import com.codename1.charts.util.ColorUtil;
 import java.util.Random; 
 
 public class Spider extends Moveable {
 
-	private int damageAmount;	
+//	private int damageAmount;	
+	
+	private static final Random rand = new Random();
 	
 	public Spider() {
 		
-		super(ColorUtil.BLACK, 25, new Random().nextInt(10) + 5, new Random().nextInt(359));
+		//super(ColorUtil.BLACK, 25, new Random().nextInt(10) + 5, new Random().nextInt(359));
+		
+		super(generateRandomSize(), generateRandomLocation(), ColorUtil.BLACK, rand.nextInt(360), 5 + rand.nextInt(11));
 		
 	}
 	
-	public int getDamageAmount() {
-		return damageAmount;
+	private static int generateRandomSize() {
+		return 10 + rand.nextInt(41);
 	}
 	
-	public void updateHeading() {
-		
+	private static Point generateRandomLocation() {
+		float x = rand.nextFloat() * 1000.0f;
+		float y = rand.nextFloat() * 1000.0f;
+		return new Point(x, y);
 	}
+	
 	
 	@Override
 	public void setFoodLevel(int fl) {
-		// dont update. Spiders should aren't foodies so they cannot update foodlevels.
+		// dont update. Spiders aren't foodies so they cannot update foodlevels.
 		System.out.println("Spiders cannot update their food levels.");
 	}
 	
@@ -31,35 +38,34 @@ public class Spider extends Moveable {
 	public void setColor(int c) {
 		// set to black no matter what c is.
 		super.setColor(ColorUtil.BLACK);
-	}
-	
-	@Override
-	public void setSize(int s) {
-		
-		// ignore s. We do not change the size to anything other than 25.
-		super.setSize(25);
-		System.out.println("Spiders cannot change size after creation.");
-	}
-	
+	}	
 	
 	@Override
 	public void move() {
 		
-		Random rand = new Random();
+        int headingChange = (rand.nextInt(11) - 5); // -5 to +5 degrees
+        this.setHeading(this.getHeading() + headingChange);
+
+        double angle = Math.toRadians(90 - this.getHeading());
+        float deltaX = (float) (Math.cos(angle) * this.getSpeed());
+        float deltaY = (float) (Math.sin(angle) * this.getSpeed());
+        
+        float newX = this.getLocation().getX() + deltaX;
+        float newY = this.getLocation().getY() + deltaY;
+
+       // Check for world boundaries. If out of bounds, change direction.
+        if (newX < 0 || newX > 1000 || newY < 0 || newY > 1000) {
+          
+            this.setHeading(this.getHeading() + 180);
+        } else {
+            
+            super.move();
+        }
 		
-		int randomNum = rand.nextInt(10);
-		
-		int newHeading = this.getHeading() + (-5 + randomNum) ;
-		
-		this.setHeading(newHeading);
-		
-		
-		// if the spiders center hits the side of the world, it changes heading and does not move out of bounds. 
-		if (this.getLocation().getX() <= 0 || this.getLocation().getX() >= 1000 || this.getLocation().getY() <= 0 || this.getLocation().getY() >= 1000) {
-			this.setHeading(this.getHeading() - 180);
-		}
-		
-		
-		// if the spider gets to the same location as the ant, it decreases the health level by 1
+	}
+	
+	@Override
+	public String toString() {
+		return "Spider: " + super.toString();
 	}
 }
